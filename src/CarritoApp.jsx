a2a8d7e27c9345cdacb99cf0a8bf4f6e36374cae
Alexis Diaz <alexisshande@gmail.com>
2024-09-23 01:25:01 -0500

    Carrito compras funcional

--- src/CarritoApp.jsx
+++ src/CarritoApp.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { ComprasPage } from "./pages/ComprasPage";
+import { CarritoPage } from "./pages/CarritoPage";
+import { ProductosProvider } from "./context/ProductosProvider";
+import { CarritoProvider } from "./context/CarritoProvider";
 
 export const CarritoApp = () => {
   return (
-    <div>CarritoApp</div>
-  )
-}
+    <ProductosProvider>
+      <CarritoProvider>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/compras" element={<ComprasPage />}></Route>
+            <Route path="/carrito" element={<CarritoPage />}></Route>
+            <Route path="/*" element={<Navigate to="" />}></Route>
+          </Routes>
+        </div>
+      </CarritoProvider>
+    </ProductosProvider>
+  );
+};
